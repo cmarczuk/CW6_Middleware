8e2dbd79d29087b48cac90bf5f2f3489a0973240
Cezary Marczuk <77850@student.pb.edu.pl>
2021-04-21 21:33:36 +0200

    Dodaj pliki projektów.

---
+++ Middleware/CustomMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CW6_Middleware.Middleware
+{
+    public class CustomMiddleware
+    {
+        public readonly RequestDelegate next;
+
+        public CustomMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, IBrowserDetector detector)
+        {
+            var browser = detector.Browser;
+
+            if (browser.Name == BrowserNames.Edge || browser.Name == BrowserNames.EdgeChromium || browser.Name == BrowserNames.InternetExplorer)
+            {
+                await httpContext.Response.WriteAsync("<html lang='pl'><head><meta charset='utf-8'></head><body><h1>Przeglądarka nie jest obsługiwana</h1></body></html>");
+            }
+            else
+            {
+                await this.next.Invoke(httpContext);
+            }
+        }
+    }
+}
